296af007e26f898c1b9a5a331cf839a4ad1a783a
VTEyin618 <123543776+VTEyin618@users.noreply.github.com>
2023-10-18 15:35:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// 实现轮播图切换
+let img = document.querySelector('.banner1');
+let slide = document.querySelector('.banner');
+
+let imgArr = ['banner1.jpg','banner2.jpg','banner3.jpg','banner4.jpg','banner5.jpg','banner6.jpg','banner7.jpg'];
+
+let count = 0;
+
+// 定义函数，用来切换图片的路径
+function cutImg(){
+    img.src = './img/' + imgArr[count];
+    for(let i = 0; i < lis.length; i++){
+        lis[i].className = '';
+    }
+    lis[count].className = 'active';
+}
+
+// 设置定时器每隔3秒count加1然后执行函数
+let timer = setInterval(function(){
+    count++;
+    if(count > imgArr.length - 1){
+        count = 0;
+    }
+    cutImg();
+}, 2000);
+
+// 鼠标滑入div，将定时切换图片给干掉
+slide.onmouseover = function(){
+    clearInterval(timer);
+}
+
+// 鼠标滑出div，将定时切换图片启动
+slide.onmouseout = function(){
+    timer = setInterval(function(){
+        count++;
+        if(count > imgArr.length - 1){
+            count = 0;
+        }
+        cutImg();
+    }, 2000);
+}
---
+++ login.js
@@ -0,0 +1,15 @@
+$(function() { 
+	$('.body-btn').click(function() {//为登录按钮定义点击事件
+		if ($('#zhang').val() != '' && $('#mima').val() != '') {//判断输入框是否为空
+			if ($('#zhang').val() == 'admin' && $('#mima').val() == 'admin123') {//判断用户输入的账号密码是否正确
+				alert('登录成功！');//弹窗提示登录成功
+				location.href = 'index.html';//登录成功后跳到首页
+			} else {
+				alert('账号或密码输入错误！');//提示账号或密码输入错误
+			} 
+		} else {
+			alert('文本框不能为空！');//提示文本框不能为空
+		}
+	})
+})  
+ 
